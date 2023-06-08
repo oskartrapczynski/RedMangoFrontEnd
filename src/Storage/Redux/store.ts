7e86b2bd32a652d93f4ba01a82543c8387d530b6
Oskar Trapczynski <oskardenis167@gmail.com>
2023-06-08 14:40:04 +0200

    setup redux query

--- src/Storage/Redux/store.ts
+++ src/Storage/Redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { menuItemApi } from '../../Apis';
 import { menuItemReducer } from './menuItemSlice';
 
 const store = configureStore({
   reducer: {
     menuItemStore: menuItemReducer,
+    [menuItemApi.reducerPath]: menuItemApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(menuItemApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
